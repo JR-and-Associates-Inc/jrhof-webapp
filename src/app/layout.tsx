7e0003e1822f60af660b3bc91784bd05efec0f26
TJ Olnhausen <tj@tmcoconsulting.com>
2025-05-05 09:55:13 -0600

    Updated favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,9 +30,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Analytics />
         <Clarity />
         <link rel="icon" href="/favicon/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
         <link rel="apple-touch-icon" href="/favicon/apple-touch-icon.png" />
         <link rel="icon" sizes="192x192" href="/favicon/android-chrome-192x192.png" />
         <link rel="icon" sizes="512x512" href="/favicon/android-chrome-512x512.png" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <meta name="apple-mobile-web-app-title" content="JRHOF" />
+        <meta name="application-name" content="JRHOF" />
         <meta name="theme-color" content="#ffffff" />
       </head>
       <body
