411a29de5507cdd216481a2673a28a09cb752a99
TJ Olnhausen <tj@tmcoconsulting.com>
2025-07-02 16:58:48 -0600

    Added 2023 & 24 HOF Banquet

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -29,6 +29,14 @@ export default function EventsPage() {
             <h2 className="text-xl font-bold mb-2 text-center">2024 Golf Tournament</h2>
             <p className="text-gray-700 dark:text-gray-300">See event info and browse the gallery for the 2024 Umpire&apos;s Cup II Golf Tournament.</p>
           </Link>
+          <Link href="/events/2024/hof-banquet" className="block p-4 rounded-[8px] shadow-[0_4px_10px_rgba(0,0,0,0.15)] hover:shadow-lg hover:translate-y-[-2px] hover:ring-2 hover:ring-[#0078D7] transition bg-white/80 dark:bg-[#333]">
+            <h2 className="text-xl font-bold mb-2 text-center">2024 HOF Induction Banquet</h2>
+            <p className="text-gray-700 dark:text-gray-300">See event info and browse the gallery for the 2024 Hall of Fame Induction Banquet.</p>
+          </Link>
+          <Link href="/events/2023/hof-banquet" className="block p-4 rounded-[8px] shadow-[0_4px_10px_rgba(0,0,0,0.15)] hover:shadow-lg hover:translate-y-[-2px] hover:ring-2 hover:ring-[#0078D7] transition bg-white/80 dark:bg-[#333]">
+            <h2 className="text-xl font-bold mb-2 text-center">2023 HOF Induction Banquet</h2>
+            <p className="text-gray-700 dark:text-gray-300">See event info and browse the gallery for the 2023 Hall of Fame Induction Banquet.</p>
+          </Link>
           <Link href="/events/2010/hof-banquet" className="block p-4 rounded-[8px] shadow-[0_4px_10px_rgba(0,0,0,0.15)] hover:shadow-lg hover:translate-y-[-2px] hover:ring-2 hover:ring-[#0078D7] transition bg-white/80 dark:bg-[#333]">
             <h2 className="text-xl font-bold mb-2 text-center">2010 HOF Induction Banquet</h2>
             <p className="text-gray-700 dark:text-gray-300">See event info and browse the gallery for the 2010 Hall of Fame Induction Banquet.</p>
